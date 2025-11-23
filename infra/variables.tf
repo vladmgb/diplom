0f9ec4bfbce8196df9c5140f2e8270579cac7279
vladmgb <vladmgb@gmail.com>
2025-11-23 19:26:22 +0500

    ok

--- infra/variables.tf
+++ infra/variables.tf
@@ -17,7 +17,7 @@ variable "zone" {
 }
 
 variable "subnets" {
-  description = "Subnets configuration for 3 zones"
+  description = "Subnets configuration for zones"
   type = map(object({
     zone = string
     cidr = string
